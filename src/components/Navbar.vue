ea387de0b251a3da255bfbaba7b7e3319681538d
初言 <xiejiadi@bilibili.com>
2024-08-06 21:22:59 +0800

    update

---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <a href="/">首页</a>
+        <a href="/about">关于我</a>
+        <a href="/blog">博客</a>
+    </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style>
+</style>
