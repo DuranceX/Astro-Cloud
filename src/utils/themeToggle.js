d9609c5383e676dc1d09462da7283c34c2094511
初言 <xiejiadi@bilibili.com>
2024-08-16 17:32:43 +0800

    两个暗色模式切换按钮合一

--- src/utils/themeToggle.js
+++ src/utils/themeToggle.js
@@ -1,27 +1,13 @@
 // theme.js
-document.addEventListener('DOMContentLoaded', () => {
-  const htmlElement = document.documentElement;
-  const themeToggle1 = document.getElementById('dark-mode-checkbox');
-  const themeToggle2 = document.getElementById('dark-mode-checkbox-2');
-
   // 从 localStorage 获取主题状态
-  const isDark = localStorage.getItem('theme') === 'dark';
+const isDark = localStorage.getItem('theme') === 'dark';
 
-  // 根据状态添加或移除 dark 类
-  if (isDark) {
-      htmlElement.classList.remove('light');
-      htmlElement.classList.add('dark');
-      themeToggle1.checked = true;
-      themeToggle2.checked = true;
-  }else{
-      htmlElement.classList.remove('dark');
-      htmlElement.classList.add('light');
-      themeToggle1.checked = false;
-      themeToggle2.checked = false;
-  }
+const htmlElement = document.documentElement;
+const switchBtns = document.querySelectorAll('div[data-name]')
 
-  const switchBtn = document.getElementById('switchBtn');
-  const switchBtn2 = document.getElementById('switchBtn2');
+switchBtns.forEach((switchBtn) => {
+  // 根据状态设置按钮初始状态
+  switchBtn.firstElementChild.checked = isDark;
   switchBtn.addEventListener('change', () => {
     if (htmlElement.classList.contains('dark')) {
       htmlElement.classList.remove('dark');
@@ -33,15 +19,4 @@ document.addEventListener('DOMContentLoaded', () => {
       localStorage.setItem('theme', 'dark');
     }
   });
-  switchBtn2.addEventListener('change', () => {
-    if (htmlElement.classList.contains('dark')) {
-      htmlElement.classList.remove('dark');
-      htmlElement.classList.add('light');
-      localStorage.setItem('theme', 'light');
-    } else {
-      htmlElement.classList.remove('light');
-      htmlElement.classList.add('dark');
-      localStorage.setItem('theme', 'dark');
-    }
-  });
-});
+})
