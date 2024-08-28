b3ab80ada1f17e2c4bc5d0d7dedaff4e032b0d2c
初言 <xiejiadi@bilibili.com>
2024-08-28 15:18:24 +0800

    feat: 完成导航栏的所有内容

--- src/utils/themeToggle.js
+++ src/utils/themeToggle.js
@@ -1,22 +1,35 @@
 // theme.js
   // 从 localStorage 获取主题状态
-const isDark = localStorage.getItem('theme') === 'dark';
+function bindThemeToggle() {
+  const isDark = localStorage.getItem('theme') === 'dark';
 
-const htmlElement = document.documentElement;
-const switchBtns = document.querySelectorAll('div[data-name]')
+  const htmlElement = document.documentElement;
+  const switchBtns = document.querySelectorAll('div[data-name]')
 
-switchBtns.forEach((switchBtn) => {
-  // 根据状态设置按钮初始状态
-  switchBtn.firstElementChild.checked = isDark;
-  switchBtn.addEventListener('change', () => {
-    if (htmlElement.classList.contains('dark')) {
-      htmlElement.classList.remove('dark');
-      htmlElement.classList.add('light');
-      localStorage.setItem('theme', 'light');
-    } else {
-      htmlElement.classList.remove('light');
-      htmlElement.classList.add('dark');
-      localStorage.setItem('theme', 'dark');
-    }
-  });
-})
+  if(isDark){
+    htmlElement.classList.remove('light');
+    htmlElement.classList.add('dark');
+  }else{
+    htmlElement.classList.remove('dark');
+    htmlElement.classList.add('light');
+  }
+
+  switchBtns.forEach((switchBtn) => {
+    // 根据状态设置按钮初始状态
+    switchBtn.firstElementChild.checked = isDark;
+    switchBtn.addEventListener('change', () => {
+      if (htmlElement.classList.contains('dark')) {
+        htmlElement.classList.remove('dark');
+        htmlElement.classList.add('light');
+        localStorage.setItem('theme', 'light');
+      } else {
+        htmlElement.classList.remove('light');
+        htmlElement.classList.add('dark');
+        localStorage.setItem('theme', 'dark');
+      }
+    });
+  })
+}
+
+// window.addEventListener('load', bindThemeToggle);
+document.addEventListener('astro:page-load', bindThemeToggle);
